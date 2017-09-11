b80031c8d5ed0549fb6dec565a7672a3016eb67b
Jade Bautista <edaj.bautista@gmail.com>
2017-09-11 21:29:11 +0800

    Add route.js

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const router = require('./route');
 
 app.use(morgan('combined')); // for log in of user info
 app.use(bodyParser.json({ type: '*/*' })); //json parser
+router(app);
 
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
