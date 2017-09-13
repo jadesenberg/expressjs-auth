2ca79425c8063d9eb49632f2459e8de8e195bd62
Jade Bautista <edaj.bautista@gmail.com>
2017-09-14 00:06:21 +0800

    User creating process

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -5,7 +5,26 @@ exports.signup = function(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
 
+    //check if user exist
     User.findOne({ email: email }, function(err, existingUser) { //find email then return existingUser variable
+        if(err) { return next(err); }
 
+        //if user email exist
+        if(existingUser){
+            return res.status(422).send({ error: 'Email already exist' });
+        }
+
+        //if user email not exist
+        const user = new User({
+            email: email,
+            password: password
+        });
+
+        user.save(function(err) {
+            if(err) { return next(err); }
+
+            //response to request
+            res.json(user);
+        });
     });
 }
