4656663a01c04313777ed9e2c7de208150cb063f
Jade Bautista <edaj.bautista@gmail.com>
2017-09-28 14:24:42 +0800

    Setup cors library

--- server/index.js
+++ server/index.js
@@ -5,11 +5,13 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 //db setup
 mongoose.connect('mongodb://localhost/auth',{ useMongoClient: true });
 
 app.use(morgan('combined')); // for log in of user info
+app.use(cors()); // for cors
 app.use(bodyParser.json({ type: '*/*' })); //json parser
 router(app);
 
